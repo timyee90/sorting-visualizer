b1e5c8b115d51039db21b48717321500d2235e76
Tim <timyee90@gmail.com>
2020-05-08 17:22:06 -0400

    change output filepath

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
   entry: './client/app.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './public/dist'),
     filename: 'bundle.js',
   },
   module: {
